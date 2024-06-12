cd39da8ccdcd952eead1b39020cd7c1f2db88910
Davin <85967571+dav1n9@users.noreply.github.com>
2024-06-12 14:01:18 +0900

    database 설정 추가

--- app.py
+++ app.py
@@ -1,5 +1,30 @@
 from flask import Flask, render_template
+import os
+from flask_sqlalchemy import SQLAlchemy
+
+basedir = os.path.abspath(os.path.dirname(__file__))
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] =\
+        'sqlite:///' + os.path.join(basedir, 'database.db')
+
+db = SQLAlchemy(app)
+
+class Song(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    movie_cd = db.Column(db.String, nullable=False)
+    username = db.Column(db.String, nullable=False)
+    content = db.Column(db.String, nullable=False)
+    rating = db.Column(db.Integer, nullable=False)
+    created_at = db.Column(db.DateTime, nullable=False)
+
+    def __repr__(self):
+        return f'{self.movie_cd} : {self.content} by {self.username}, rating: {self.rating}'
+
+with app.app_context():
+    db.create_all()
+
+
+
 
 
 if __name__ == "__main__":
