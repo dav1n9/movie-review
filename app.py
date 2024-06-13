5964462a3bbe6b94462d47da202fac52aa0e9b05
Seungwon_Choi <bestwondev@gmail.com>
2024-06-13 09:32:18 +0900

    FEAT: CGV web crawling
    CGV 영화차트 웹 크롤링

--- app.py
+++ app.py
@@ -2,10 +2,16 @@ from flask import Flask, render_template
 import os
 from flask_sqlalchemy import SQLAlchemy
 
+import urllib.request as req
+
+import requests
+from bs4 import BeautifulSoup
+
+
 basedir = os.path.abspath(os.path.dirname(__file__))
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] =\
-        'sqlite:///' + os.path.join(basedir, 'database.db')
+app.config['SQLALCHEMY_DATABASE_URI'] = \
+    'sqlite:///' + os.path.join(basedir, 'database.db')
 
 db = SQLAlchemy(app)
 
@@ -23,9 +29,33 @@ class Song(db.Model):
 with app.app_context():
     db.create_all()
 
+def get_box_office():
+    code=req.urlopen("http://www.cgv.co.kr/movies/?lt=1&ft=0")
+    soup=BeautifulSoup(code, "html.parser")
 
+    movies = []
+    movie_list = soup.select("li")
+    for movie in movie_list:
+        rank = movie.select_one("strong.rank")
+        # info = movie.select_one("div.box-contents")
+        title = movie.select_one("strong.title")
+        img_url = movie.select_one("img")
 
+        if title and img_url:  # title과 img_url이 모두 존재하는 경우에만 추가
+            movies.append({
+                'title': title.string,
+                'image': img_url.get("src"),
+                'rank': rank.string
+            })
 
+    return movies
+
+@app.route('/')
+def movie():
+    movies = get_box_office()
+
+    print(get_box_office())
+    return render_template('movie.html', movies=movies)
 
 if __name__ == "__main__":
     app.run(debug=True)
